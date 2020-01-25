0f78ce1292912e1dcea2c30187270605e21ed50d
AurioPinto <auriopinto@outlook.com>
2020-01-26 00:35:59 +0800

    Fetch API

---
+++ src/components/jikan.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="display">
+    <ul>
+      <li v-for="result in results" :key="result">
+        <input type="text" placeholder="Search" />
+        {results.text}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  components: {},
+  data() {
+    return {
+      user: {
+        name: "VueJsDialog"
+        // note: ""
+      },
+      results: []
+    };
+  },
+  methods: {
+    display() {
+      axios
+        .get(`https://api.jikan.moe/v3/search/anime?q=${this.note}&limit=20`)
+        .then(data => {
+          this.results = data.results;
+        });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.display {
+  overflow: hidden;
+  // background-color: #e9e9e9;
+
+  .display {
+    float: left;
+    display: block;
+    color: black;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+  }
+}
+</style>
---
+++ src/views/jikan.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="unix">
+    <jikan></jikan>
+  </div>
+</template>
+
+<script>
+import jikan from "../components/jikan";
+export default {
+  name: "index",
+  components: { jikan },
+  data: function() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.unix {
+  background-color: #ffffff;
+  min-height: 100vh;
+}
+</style>
