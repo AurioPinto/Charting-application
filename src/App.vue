7981522b19418cb936168332deed59c1bd73e7c1
AurioPinto <auriopinto@outlook.com>
2020-03-08 11:08:00 +0800

    webkit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      //TODO:  this is a test
+      <!-- //TODO:  this is a test -->
       <!-- <router-link to="/">Home</router-link> | -->
       <!-- <router-link to="/about">About</router-link> -->
     </div>
@@ -11,23 +11,29 @@
 
 <style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  // font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  color: #000;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+body {
+  margin: 0px;
+  font-family: "Lato" sans-serif;
+  // padding: 10px;
+}
+body::-webkit-scrollbar {
+  display: none !important;
+}
+* {
+  margin: 0;
+  padding: 0;
+  margin-block-start: 0;
+  margin-block-end: 0;
+  margin-inline-start: 0;
+  margin-inline-end: 0;
+}
+html,
+body {
+  height: 100%;
 }
 </style>
