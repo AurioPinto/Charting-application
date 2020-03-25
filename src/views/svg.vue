250bb01a51573a09200c766f6e7fae9e2df0341c
AurioPinto <auriopinto@outlook.com>
2020-03-26 06:55:43 +0800

    update usage

---
+++ src/views/svg.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="unix">
+    <svg />
+  </div>
+</template>
+
+<script>
+// import svg from "../components/svg";
+export default {
+  name: "index",
+  //   components: { svg },
+  data: function() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.unix {
+  background-color: #00263c;
+  min-height: 100vh;
+}
+</style>
