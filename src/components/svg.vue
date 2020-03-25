e4d0590e3fd39458202ad563fd72f09d9108d69e
AurioPinto <auriopinto@outlook.com>
2020-03-26 06:56:40 +0800

    layears and layers

---
+++ src/components/svg.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>
+      <span>Welcome</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "svg",
+  data() {
+    return {
+      user: {
+        name: "svg"
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
