e7b4360ad0c3a8a7ed972f5680253ce894066df8
AurioPinto <auriopinto@outlook.com>
2020-03-31 14:40:28 +0800

    remote API

--- src/components/jikan.vue
+++ src/components/jikan.vue
@@ -1,31 +1,12 @@
 <template>
   <div class="display">
+    <label>
+      <input type="text" v-model="keyWords" placeholder="Search" />
+    </label>
+    <button @click="search">SEARCH</button>
     <ul>
-      <li v-for="result in results" :key="result">
-        <input type="text" placeholder="Search" />
-        {results.text}
-      </li>
+      <li v-for="(item, $index) in results" :key="$index">{{ item.title }}}</li>
     </ul>
-    <div>{{ info }}</div>
-    <div class="columns medium-3" v-for="resultado in resulta" :key="resultado">
-      <div class="card">
-        <div class="card-divider">
-          {{ resultado.title }}
-        </div>
-        <div class="card-section">
-          <p>{{ resultado.abstract }}.</p>
-        </div>
-      </div>
-    </div>
-    <button @click="AddItem" type="button" name="button">Add</button>
-    <div v-for="(item, Itemindex) in items" :key="'item' + Itemindex">
-      <input type="text" v-model="items[Itemindex]" />(Works)
-      {{ items }}
-    </div>
-    <a v-bind:href="blog">Aurio Pinto</a>
-    <section>
-      <p>I'm centered with Flexbox!</p>
-    </section>
   </div>
 </template>
 
@@ -36,75 +17,45 @@ export default {
   data() {
     return {
       results: "",
-      info: null,
-      items: [],
-      blog: "https://www.auriopinto.com.cn/#/",
-      resulta: [
-        {
-          title: "the very first post",
-          abstract: "lorem ipsum some test dimpsum"
-        },
-        {
-          title: "and then there was the second",
-          abstract: "lorem ipsum some test dimsum"
-        },
-        {
-          title: "third time's a charm",
-          abstract: "lorem ipsum some test dimsum"
-        },
-        {
-          title: "four the last time",
-          abstract: "lorem ipsum some test dimsum"
-        }
-      ]
+      keyWords: ""
+      // info: null,
+      // items: [],
+      // blog: "https://www.auriopinto.com.cn/#/",
     };
   },
   methods: {
-    display() {
+    search() {
       axios
-        .get(`https://api.jikan.moe/v3/search/anime?q=${this.note}&limit=20`)
+        .get(
+          `https://api.jikan.moe/v3/search/anime?q=${this.keyWords}&limit=20`
+        )
         .then(data => {
           this.results = data.results;
         });
     }
-  },
-  shared: {
-    AddItem: function() {
-      this.items.push("New Value" + this.items.lenght);
-    }
-  },
-  mounted() {
-    axios
-      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
-      .then(response => (this.info = response));
   }
 };
 </script>
 
 <style lang="scss" scoped>
 .display {
-  overflow: hidden;
-  // background-color: #e9e9e9;
-
-  .display {
-    float: left;
-    display: block;
-    color: black;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    font-size: 17px;
-    section {
-      display: flex;
-      width: 50%;
-      height: 200px;
-      margin: auto;
-      border-radius: 10px;
-      border: 3px dashed #1c87c9;
-      p {
-        margin: auto; /* Important */
-        text-align: center;
-      }
+  float: left;
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  section {
+    display: flex;
+    width: 50%;
+    height: 200px;
+    margin: auto;
+    border-radius: 10px;
+    border: 3px dashed #1c87c9;
+    p {
+      margin: auto; /* Important */
+      text-align: center;
     }
   }
 }
