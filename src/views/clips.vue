7e46f836a56af58ae4b3847d6483e2a046f87954
AurioPinto <auriopinto@outlook.com>
2020-04-01 16:42:57 +0800

    tranlated

---
+++ src/views/clips.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="clips">
+    <clips />
+  </div>
+</template>
+
+<script>
+import clips from "../components/clips";
+export default {
+  name: "clips2",
+  components: { clips },
+  data: function() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.clips {
+  background-color: #ffffff;
+  min-height: 100vh;
+}
+</style>
