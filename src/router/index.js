b07e161ffdfc5dfd6939e0915b6efb80d78d6fa6
AurioPinto <auriopinto@outlook.com>
2020-08-10 01:22:18 +0800

    router added

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,22 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import svg from "../views/svg.vue";
 import Home from "../views/Home.vue";
-import value from "../views/value.vue";
+import history from "../views/history.vue";
 import jikan from "../views/jikan.vue";
-import clips from "../views/clips.vue"
+import clips from "../views/clips.vue";
+import chart from "../views/chart.vue";
+import value from "../views/value.vue";
+import research from "../views/research.vue";
+import project from "../views/project.vue";
+import swap from "../views/swap.vue";
+
+
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
+    path: "/home",
     name: "home",
     component: Home
   },
@@ -25,14 +32,39 @@ const routes = [
     component: svg
   },
   {
-    path: "/clips",
-    name: "clips",
-    component: clips
+    path: "/swap",
+    name: "swap",
+    component: swap
+  },
+  {
+    path: "/project",
+    name: "project",
+    component: project
+  },
+  {
+    path: "/research",
+    name: "research",
+    component: research
   },
   {
     path: "/value",
     name: "value",
     component: value
+  },
+  {
+    path: "/",
+    name: "clips",
+    component: clips
+  },
+  {
+    path: "/chart",
+    name: "chart",
+    component: chart
+  },
+  {
+    path: "/history",
+    name: "history",
+    component: history
   },
   {
     path: "/about",
