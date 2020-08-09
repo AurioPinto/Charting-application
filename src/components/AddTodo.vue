9cccfda553da981b1c4cb81d2f59d06217ab8fdc
AurioPinto <auriopinto@outlook.com>
2020-08-10 03:27:00 +0800

    search engine

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,58 @@
+<template>
+    <form @submit="addTodo">
+        <input type="text" name="title" placeholder="AddTodo...">
+        <input type="submit" value="submit" class="btn">
+    </form>
+</template>
+
+<script>
+    // import uuid from 'uuid';
+    export default {
+        name: "AddTodo",
+        data(){
+            return{
+                title: ''
+            }
+        },
+        methods:{
+            addTodo(e){
+                e.preventDefault();
+                const newTodo={
+                    // id: uuid.v4(),
+                    title: this.title,
+                    completed: false
+                }
+                // Send up to parent
+                this.$emit('add-todo', newTodo);
+
+                this.title='';
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    form{
+        display: flex;
+        input[type='text']{
+            flex: 10;
+            padding: 5px;
+            background: #ffffff;
+            color: #333333;
+        }
+        input[type='submit']{
+            flex: 2;
+        }
+        .btn{
+            display: inline-block;
+            border: none;
+            background: #555555;
+            color: #fff;
+            padding: 7px 20px;
+            cursor: pointer;
+        }
+        .btn:hover{
+            background: #666666;
+        }
+    }
+</style>
---
+++ src/components/todoitem.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <p>
+        <input type="checkbox" v-on:change="markcomplete">
+        {{todo.title}}
+        <button @click="$emit('del-todo', todo.id)" class="del">X</button>
+    </p>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "todoitem",
+        props:["todo"],
+        methods:{
+            markcomplete: function () {
+                this.todo.completed = !this.todo.completed
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.todo-item{
+    background-color: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #cccccc dotted;
+}
+    .is-complete{
+    text-decoration: line-through;
+    }
+    .del{
+        background-color: #ff0000;
+        color: #ffff;
+        border: none;
+        padding: 5px 9px;
+        border-radius: 50px;
+        cursor: pointer;
+        float: right;
+    }
+
+</style>
